3d203bc4ddff3751da5a0493125f71bebaea144e
cs-gregg <greg.greene@cigarettestore.com>
2025-03-30 12:05:29 -0400

    Updates to form settings and their variables

--- js/sf.paytrace.js
+++ js/sf.paytrace.js
@@ -7,22 +7,57 @@ sf.paytrace.state = {
 };
 
 sf.paytrace.forms = {
+	'ui': {
+		'overlay': {
+			'id': '',
+		}
+	},
    'protect': {
     'id': 'ProtectForm',
-    'buttons': {
-      'submit': {
-        'id': '',
-        'onclick': {}
-      },
-      'cancel': {
-        'id': '',
-        'onclick': {}
-      },
-      'finish': {
-        'id': '',
-        'onclick': {}
-      }
-   }
+   },
+	'ui': {
+		'id': '',
+		'elements': {
+			'outerContainer' : {
+				'id': ''
+			},
+			'hpfForm' : {
+				'id': 'pt_hpf_form'
+			},
+			'titleContainer' : {
+				'id': ''
+			},
+			'title' : {
+				'id': ''
+			},
+			'messageContainer' : {
+				'id': ''
+			},
+			'message' : {
+				'id': ''
+			},
+			'amountContainer' : {
+				'id': ''
+			},
+			'amount' : {
+				'id': ''
+			},
+			 'buttons': {
+			      'submit': {
+			        'id': '',
+			        'onclick': {}
+			      },
+			      'cancel': {
+			        'id': '',
+			        'onclick': {}
+			      },
+			      'finish': {
+			        'id': '',
+			        'onclick': {}
+			      }
+			 }
+		}
+	}
 };
 
 sf.paytrace.request = {
@@ -75,22 +110,22 @@ sf.paytrace.processProtectAuth = function(hpfToken, encKey, ptProtectToken) {
     }
   
     var url = sf.paytrace.baseEndpointUri + 'postPtProtectAuth.php',
-    data = {},
-    addressParts = '',
-    amount = $('#' + sf.paytrace.request.payment_fields.amount.id).val(),
-    name = $('#' + sf.paytrace.request.payment_fields.name.id).val(),
-    addressStreet = $('#' + sf.paytrace.request.payment_fields.street.id).val(),
-    addressCityStateZipcode = $('#' + sf.paytrace.request.payment_fields.city_state_postal_code.id).val(),
-    zipcode = $('#' + sf.paytrace.request.payment_fields.postal_code.id).val(),
-    country = $('#' + sf.paytrace.request.payment_fields.country.id).val(),
-    emailAddress = $('#' + sf.paytrace.request.payment_fields.emailaddress.id).val(),
-    phone = $('#' + sf.paytrace.request.payment_fields.phone.id).val(),
-    ptForm = document.getElementById("ProtectForm"),
-	  pfFormSubmitButton = $('#' + sf.paytrace.buttons.submit.id).val(),
-    pfFormCancelButton = $('#' + sf.paytrace.buttons.cancel.id).val(),
-    pfFormFinishButton = $('#' + sf.paytrace.buttons.finish.id).val(),
-    detail = '',
-    description = '';
+	data = {},
+	addressParts = '',
+	amount = $('#' + sf.paytrace.request.payment_fields.amount.id).val(),
+	name = $('#' + sf.paytrace.request.payment_fields.name.id).val(),
+	addressStreet = $('#' + sf.paytrace.request.payment_fields.street.id).val(),
+	addressCityStateZipcode = $('#' + sf.paytrace.request.payment_fields.city_state_postal_code.id).val(),
+	zipcode = $('#' + sf.paytrace.request.payment_fields.postal_code.id).val(),
+	country = $('#' + sf.paytrace.request.payment_fields.country.id).val(),
+	emailAddress = $('#' + sf.paytrace.request.payment_fields.emailaddress.id).val(),
+	phone = $('#' + sf.paytrace.request.payment_fields.phone.id).val(),
+	ptForm = document.getElementById(sf.paytrace.forms.protect.id),
+	ptFormSubmitButton = $('#' + sf.paytrace.buttons.submit.id).val(),
+	ptFormCancelButton = $('#' + sf.paytrace.buttons.cancel.id).val(),
+	ptFormFinishButton = $('#' + sf.paytrace.buttons.finish.id).val(),
+	detail = '',
+	description = '';
     
     addressParts = !addressCityStateZipcode ? '' : addressCityStateZipcode.split(',');
     country = country != '' ? country : 'US';
@@ -147,41 +182,41 @@ sf.paytrace.processProtectAuth = function(hpfToken, encKey, ptProtectToken) {
         // console.debug('data: ' + JSON.stringify(data));
         
         var responseStatus = data['status'],
-        	responseStatusCode = data['status_code'],
-            responseCode = data['data']['response_code'],
-            responseMessage = data['data']['response_message'],
-            transactionId = '',
-            approvalMessage = '',
-            formMessage = $('#pt_form_message');
+		responseStatusCode = data['status_code'],
+		responseCode = data['data']['response_code'],
+		responseMessage = data['data']['response_message'],
+		transactionId = '',
+		approvalMessage = '',
+		formMessage = $('#' + sf.paytrace.forms.protect.ui.elements.message.id);
         
         if(responseStatus != 'success') {
-        	if('length' in data['data']) {
-            	if(data['data'][0]) {
-            		  detail = data['data'][0]['detail'];
-              }
-            }
+		if('length' in data['data']) {
+			if(data['data'][0]) {
+				detail = data['data'][0]['detail'];
+			}
+		}
 
-            // DEBUG
-        	  // console.debug('An error has occurred (' + responseStatusCode + ')');
-            formMessage.addClass('red');
-            formMessage.removeClass('green');
-            formMessage.text('An error has occurred. Please try again. ' + ((!responseStatusCode)?'':' (' + responseStatusCode + ') ') + detail);
-            return;
+		// DEBUG
+		// console.debug('An error has occurred (' + responseStatusCode + ')');
+		formMessage.addClass('red');
+		formMessage.removeClass('green');
+		formMessage.text('An error has occurred. Please try again. ' + ((!responseStatusCode)?'':' (' + responseStatusCode + ') ') + detail);
+		return;
         }
         
         if(responseCode == 101) {
-        	transactionId = data['data'].transaction_id;
-            formMessage.addClass('green');
-            formMessage.removeClass('red');
-            formMessage.text('Payment successful. Thank you! Transaction ID: ' + transactionId);
-        	pfFormSubmitButton.addClass('hide');
-   		 	pfFormCancelButton.addClass('hide');
-    		pfFormFinishButton.removeClass('hide');
+		transactionId = data['data'].transaction_id;
+		formMessage.addClass('green');
+		formMessage.removeClass('red');
+		formMessage.text('Payment successful. Thank you! Transaction ID: ' + transactionId);
+		ptFormSubmitButton.addClass('hide');
+		ptFormCancelButton.addClass('hide');
+		ptFormFinishButton.removeClass('hide');
         }
         else {
-        	approvalMessage = data['data'].approval_message;
-            formMessage.addClass('red');
-            formMessage.text('There was a problem processing your transaction: ' + approvalMessage);
+		approvalMessage = data['data'].approval_message;
+		formMessage.addClass('red');
+		formMessage.text('There was a problem processing your transaction: ' + approvalMessage);
         }
 
         // DEBUG
@@ -189,31 +224,31 @@ sf.paytrace.processProtectAuth = function(hpfToken, encKey, ptProtectToken) {
       	// alert('status:' + status);
       },
       fail: function(xhr, textStatus, errorThrown) {
-		    // alert('request failed');
+	// alert('request failed');
       }
     })
     .done(function( data ) {
         // DEBUG
         // console.debug('done: ' + data);
-  	})
+  })
     .fail(function( jqXHR, textStatus, errorThrown ) {
         alert('error(fail): ' + errorThrown + '; status:' + jqXHR.status + ', textStatus:' + textStatus);
-  	});
+  });
     
     // DEBUG
     // alert('posting complete');  
 };
 
 sf.paytrace.finishPtProtect = function() {
-	var formOuter = $('#pt_protect_form_outer');
-    formOuter.addClass('hide');
-    $(document.body).removeClass('mode--modal');
+	var formOuter = $('#' + sf.paytrace.forms.protect.ui.elements.outerContainer.id);
+	formOuter.addClass('hide');
+	$(document.body).removeClass('mode--modal');
 };
 
 sf.paytrace.cancelPtProtect = function() {
-	var formOuter = $('#pt_protect_form_outer');
-    formOuter.addClass('hide');
-    $(document.body).removeClass('mode--modal');
+	var formOuter = $('#' + sf.paytrace.forms.protect.ui.elements.outerContainer.id);
+	formOuter.addClass('hide');
+	$(document.body).removeClass('mode--modal');
 };
 
 sf.paytrace.initPtProtect = function(f_success, f_fail) {
@@ -223,13 +258,13 @@ sf.paytrace.initPtProtect = function(f_success, f_fail) {
   
 	var request = '';
 
-    var overlay = document.getElementById('pt_overlay'),
-        documentBody = $(document.body),
-        formOuter = $('#pt_protect_form_outer'),
-    	ptProtectToken = '',
-        amount = $('#input_10_10').val();
+	var overlay = document.getElementById(sf.paytrace.forms.ui.overlay.id),
+		documentBody = $(document.body),
+		formOuter = $('#' + sf.paytrace.forms.protect.ui.elements.outerContainer.id),
+		ptProtectToken = '',
+		amount = $('#' + sf.paytrace.request.payment_fields.amount.id).val();
     
-    $('#pt_form_message').text('');
+    $('#' + sf.paytrace.forms.protect.ui.elements.message.id).text('');
     
     documentBody.addClass('cover');
     documentBody.addClass('mode--modal');
@@ -239,7 +274,7 @@ sf.paytrace.initPtProtect = function(f_success, f_fail) {
         overlay = $(overlay);
         overlay.addClass('modal-overlay');
         overlay.addClass('cover');
-        overlay.prop('id', 'pt_overlay');
+        overlay.prop('id', sf.paytrace.forms.ui.overlay.id);
         documentBody.append(overlay);
     }
     else {
@@ -252,27 +287,28 @@ sf.paytrace.initPtProtect = function(f_success, f_fail) {
     // DEBUG
     // alert('overlay');
     
-    $(window).on('resize load scroll orientationchange', function () {
-    	var w_width = $(window).width();
-        var w_height = $(window).height();
-        var w_scrollTop = window.scrollY;
+	$(window).on('resize load scroll orientationchange', function () {
+	    	var w_width = $(window).width();
+	        var w_height = $(window).height();
+	        var w_scrollTop = window.scrollY;
+	
 		formOuter.css('left', w_width / 4  + 'px');
-        formOuter.css('top', w_scrollTop  + 'px');
-        formOuter.css('height', w_height + 'px');
-        
-        if(w_width < 550) {
-        	formOuter.css('left', 0  + 'px');
-            formOuter.css('width', '100%');
-        }
-        else {
-        	formOuter.css('left', w_width / 4  + 'px');
-            formOuter.css('width', '80%');
-        }
-        
-        // DEBUG
-        // console.debug(w_width);
-        // console.debug(w_height);
-        // console.debug(w_scrollTop);
+	        formOuter.css('top', w_scrollTop  + 'px');
+	        formOuter.css('height', w_height + 'px');
+	        
+	        if(w_width < 550) {
+		        formOuter.css('left', 0  + 'px');
+		        formOuter.css('width', '100%');
+	        }
+	        else {
+			formOuter.css('left', w_width / 4  + 'px');
+			formOuter.css('width', '80%');
+	        }
+	        
+	        // DEBUG
+	        // console.debug(w_width);
+	        // console.debug(w_height);
+	        // console.debug(w_scrollTop);
 	});
     
     // trigger scroll event to repaint UI
@@ -290,12 +326,11 @@ sf.paytrace.initPtProtect = function(f_success, f_fail) {
     // console.debug(overlay);
 
 	// change to: getPTProtectToken.php
-	$.post(sf.paytrace.baseEndpointUri + 'getPtProtectToken.php', request, 
-    function (data) {
-      // DEBUG
-			// console.debug('post (pay):' + data);
-			// document.getElementById('token').value = data;
-			ptProtectToken = data;
+	$.post(sf.paytrace.baseEndpointUri + 'getPtProtectToken.php', request, function (data) {
+      		// DEBUG
+		// console.debug('post (pay):' + data);
+		// document.getElementById('token').value = data;
+		ptProtectToken = data;
 			
             // DEBUG
             // console.debug('ptProtectToken:');
@@ -303,7 +338,7 @@ sf.paytrace.initPtProtect = function(f_success, f_fail) {
             
             	  // Minimal Protect.js setup call
                   PTPayment.setup({
-                  		styles:
+                  	styles:
                           {
                            'code': {
                             'font_color':'#5D99CA',
@@ -395,36 +430,35 @@ sf.paytrace.initPtProtect = function(f_success, f_fail) {
                         clientKey: ptProtectToken
                       }
                   }).then(function(instance) {
-                  	//use instance object to process and tokenize sensitive data payment fields.
-                    // this can be any event we chose. We will use the submit event and stop
-                    // any default event handling and prevent event handling bubbling.
+			// use instance object to process and tokenize sensitive data payment fields.
+			// this can be any event we chose. We will use the submit event and stop
+			// any default event handling and prevent event handling bubbling.
 			
             
-var ptForm = document.getElementById(sf.paytrace.forms.protect.id),
-	  pfFormSubmitButton = document.getElementById(sf.paytrace.forms.protect.buttons.submit),
-    pfFormCancelButton = document.getElementById(sf.paytrace.forms.protect.buttons.cancel),
-    pfFormFinishButton = document.getElementById(sf.paytrace.forms.protect.buttons.finish);
+			var ptForm = document.getElementById(sf.paytrace.forms.protect.id),
+				ptFormSubmitButton = document.getElementById(sf.paytrace.forms.protect.buttons.submit),
+				ptFormCancelButton = document.getElementById(sf.paytrace.forms.protect.buttons.cancel),
+				ptFormFinishButton = document.getElementById(sf.paytrace.forms.protect.buttons.finish);
+			
+			
+			var submitPayment = function(response, token) {
+				// DEBUG
+				// console.debug('submit payment: ' + JSON.stringify(response) + ';' + token);
+				var success = response.success,
+				hpfToken = response.message['hpf_token'],
+				encKey = response.message['enc_key'];
+				
+				if(success === true) {
+					sf.paytrace.processProtectAuth(hpfToken, encKey, token);
+				}
+		};
 
-
-var submitPayment = function(response, token) {
-	// DEBUG
-	// console.debug('submit payment: ' + JSON.stringify(response) + ';' + token);
-    var success = response.success,
-    	hpfToken = response.message['hpf_token'],
-    	encKey = response.message['enc_key'];
-    
-    if(success === true) {
-    	sf.paytrace.processProtectAuth(hpfToken, encKey, token);
-    }
-};
-
-var handleError = function(error) {
+	var handleError = function(error) {
 	var formMessage = $('#pt_form_message'),
-    	message = 'Error: Please fill in all of the fields in the main form, and be sure formatting is correct. You will have to click CANCEL on this payment form to do so. ';
-    
-    formMessage.addClass('red');
-    
-    formMessage.text(message);
+	
+	message = 'Error: Please fill in all of the fields in the main form, and be sure formatting is correct. You will have to click CANCEL on this payment form to do so. ';
+	formMessage.addClass('red');
+	formMessage.text(message);
 };
 
             pfFormSubmitButton.addEventListener("click", function(e) {
@@ -466,11 +500,12 @@ var handleError = function(error) {
 			}
 		})
 		.fail(function(data) {
+			// DEBUG
 			console.debug('fail (pay):' + JSON.stringify(data));
 			if(typeof(f_fail) == 'function') {
 				f_fail(data);
 			}
-	});
+		});
     
     PTPayment.theme('horizontal');
 };
